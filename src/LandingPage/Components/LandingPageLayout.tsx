5292ff40e66e2e490afdabc54187727d7e843719
HamidReza <mazinanihamidreza80@gmail.com>
2024-07-15 12:29:51 +0330

    import benefit component

--- src/LandingPage/Components/LandingPageLayout.tsx
+++ src/LandingPage/Components/LandingPageLayout.tsx
@@ -1,6 +1,7 @@
 import Header from "./Header/Header"
 import Hero from "./Hero/Hero"
 import ButtonGradient from '../../assets/svg/ButtonGradient'
+import Benefits from "./Benefits/Benefits"
 
 const LandingPageLayout = () => {
     return (
@@ -8,6 +9,7 @@ const LandingPageLayout = () => {
             <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
                 <Header />
                 <Hero />
+                <Benefits/>
             </div>
             <ButtonGradient />
         </>
