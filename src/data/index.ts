d772d62c66143c5f438f8518d7697a673fbabb4a
HamidReza <mazinanihamidreza80@gmail.com>
2024-07-18 14:22:12 +0330

    import pricing data

--- src/data/index.ts
+++ src/data/index.ts
@@ -202,3 +202,50 @@ export const brainwaveServices: BrainwaveServices [] = [
     title:"Seamless Integration"
   },
 ];
+
+// Pricing
+
+type Pricing = {
+  id: number
+  title: string
+  description: string
+  price: number | null
+  features: string[]
+}
+
+  
+export const pricing: Pricing [] = [
+  {
+    id: 0,
+    title: "Basic",
+    description: "AI chatbot, personalized recommendations",
+    price: 1,
+    features: [
+      "An AI chatbot that can understand your queries",
+      "Personalized recommendations based on your preferences",
+      "Ability to explore the app and its features without any cost",
+    ],
+  },
+  {
+    id: 1,
+    title: "Premium",
+    description: "Advanced AI chatbot, priority support, analytics dashboard",
+    price: 9.99,
+    features: [
+      "An advanced AI chatbot that can understand complex queries",
+      "An analytics dashboard to track your conversations",
+      "Priority support to solve issues quickly",
+    ],
+  },
+  {
+    id: 2,
+    title: "Enterprise",
+    description: "Custom AI chatbot, advanced analytics, dedicated account",
+    price: null,
+    features: [
+      "An AI chatbot that can understand your queries",
+      "Personalized recommendations based on your preferences",
+      "Ability to explore the app and its features without any cost",
+    ],
+  },
+];
